47593a6c9099a60cab1b3a785ae19b8556455bd3
shashi-faced <shashikumarverma409@gmail.com>
2021-12-09 16:26:15 +0530

    7th commit

---
+++ src/app/config/urls.ts
@@ -0,0 +1,8 @@
+export class BaseUrl{
+    public BASE_URL = {
+        POST_API_URL: 'https://jsonplaceholder.typicode.com/post',
+        COMMENTS_API_URL: 'https://jsonplaceholder.typicode.com/comments'
+    }
+  static BASE_URL: any;
+
+}
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BaseUrl } from './config/urls';
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+
+  // postUrl: string = BaseUrl.BASE_URL.POST_API_URL;
 
   postUrl: string = 'https://jsonplaceholder.typicode.com/posts';
   commentUrl: string = 'https://jsonplaceholder.typicode.com/comments';
